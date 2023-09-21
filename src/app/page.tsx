ad1139c7b9791970b21b6e34fc5605bfd0fe6805
Elias <eliaskalan@yahoo.gr>
2023-09-21 15:00:04 +0300

    contend added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,28 @@ export default function Home() {
   return (
     <main className={layout.mainContainer}>
       <h1>hey, I&apos;m Ilias 👋</h1>
-      <p>I&apos;m a frontend developer, optimist, and community builder. I currently work as the VP of Developer Experience at 
-        <span className={layout.iconSpace}>
-          <Badge img='./favicon.ico' link='https://vercel.com/'>Vercel</Badge>
-        </span>
-        , where I lead the community for
-        <span className={layout.iconSpace}>
-          <Badge img='./favicon.ico' link='https://vercel.com/'>Vercel</Badge>
-        </span>
-        , an open-source web framework built with 
-        <span className={layout.iconSpace}>
-          <Badge img='./favicon.ico' link='https://vercel.com/'>Vercel</Badge>
-        </span>
+      <p className={styles.lineSpacing}>
+      Hello there, prospective collaborator! 🚀 I&apos;m Elias Kalantzis, an aspiring developer who&apos;s passionate about merging creativity with technology. Allow me to paint a picture: I&apos;m a student at Athens University of Economics and Business by day, 
+      and by night, I weave the intricate tapestry of web development using
+        <span className={styles.iconSpace}>
+          <Badge img='./images/icons/html_icon.png' link='https://vercel.com/'>HTML</Badge>
+        </span> 
+        <span className={styles.iconSpace}>
+          <Badge img='./images/icons/css_icon.png' link='https://vercel.com/'>CSS</Badge>
+        </span> 
+        <span className={styles.iconSpace}>
+          <Badge img='./images/icons/js_icon.png' link='https://vercel.com/'>JavaScript</Badge>
+        </span> 
+        and
+        <span className={styles.iconSpace}>
+          <Badge img='./images/icons/nextjs_icon.png' link='https://vercel.com/'>Next.js</Badge>
+        </span> 
+        or
+        <span className={styles.iconSpace}>
+          <Badge img='./images/icons/nodejs_icon.png' link='https://vercel.com/'>Node.js</Badge>
+        </span> 
+        My academic journey in Computer Science has been a thrilling adventure, marked by consistently high grades and ambitious aspirations. Yet, beneath the academic facade, 
+        I harbor a deep passion for photography and the adrenaline rush of Track and Field. Rest assured, I&apos;m not your typical developer. Let&apos;s delve into my world of skills and experiences.
       </p>
       <div className={styles.container}>
         <div className={styles.gallery}>
@@ -46,7 +56,27 @@ export default function Home() {
       </div>
       <div>
         <p>
-        &quot;I create educational content for developers, teaching them about web development, JavaScript and TypeScript, React and Next.js, and more. This comes in all forms: blog posts, videos, tweets, conference talks, and workshops. You can watch some of my favorites below.&quot;
+        <span className={styles.iconSpace}>
+          <Badge img='./images/icons/java_icon.png' link='https://vercel.com/'>Java</Badge>
+        </span>
+        <span className={styles.iconSpace}>
+          <Badge img='./images/icons/python_icon.png' link='https://vercel.com/'>Python</Badge>
+        </span>
+        <span className={styles.iconSpace}>
+          <Badge img='./images/icons/git_icon.png' link='https://vercel.com/'>Git</Badge>
+        </span>   
+          – these aren&apos;t just buzzwords to me; they&apos;re essential tools in my digital arsenal! 💻 I&apos;ve honed my coding skills to mastery and find joy in seamlessly maneuvering between these programming languages. 
+          <span className={styles.iconSpace}>
+            <Badge img='./images/icons/sql_icon.png' link='https://vercel.com/'>SQL</Badge>
+          </span> 
+          It&apos;s my native tongue, and I&apos;m equally fluent in
+          <span className={styles.iconSpace}>
+            <Badge img='./images/icons/postgresql_icon.png' link='https://vercel.com/'>PostgreSQL</Badge>
+          </span>
+           and 
+          <span className={styles.iconSpace}>
+            <Badge img='./images/icons/mysql_icon.png' link='https://vercel.com/'>MySQL</Badge>
+          </span>
         </p>
       </div>
       <div className={styles.containerLinkedCards}>
@@ -54,17 +84,25 @@ export default function Home() {
         <LinkCard img1='./images/linkedin-96.png' link='https://www.linkedin.com/in/ilias-kalantzis/' followers='3 followers'> Ilias Kalantzis </LinkCard>
       </div>
       <div>
-        <p>
-        &quot;Over the past decade, I&apos;ve written content on my blog and newsletter. I try to keep things simple. You&apos;ll find writing about technologies I&apos;m interested in at the time, or how I&apos;m learning and growing in my career, sharing knowledge along the way.&quot;
+        <p className={styles.lineSpacing}>
+        "In the realm of my standout projects, I&apos;ve ventured into the dynamic worlds of Android development and Kotlin expertise while weaving a passion for user-centered design. 
+        First, as part of my distributed systems coursework, I crafted a robust Java-based backend to power a distributed messaging app for group chats, seamlessly connecting with an 
+        <span className={styles.iconSpace}>
+            <Badge img='./images/icons/android_icon.png' link='https://vercel.com/'>Android</Badge>
+        </span>
+        Android application and emphasizing a user-friendly UI for seamless interactions. 
+        Additionally, I harnessed the versatility of 
+        <span className={styles.iconSpace}>
+            <Badge img='./images/icons/kotlin_icon.png' link='https://vercel.com/'>Kotlin</Badge>
+        </span>
+        Kotlin to reimagine a real dishwasher&apos;s user interface, with a keen focus on enhancing usability, following a meticulous helical model development approach.  Whether you seek a skilled Android developer, a Kotlin enthusiast, or a UX/UI design aficionado, 
+        I&apos;m poised to embark on your next tech endeavor. Let&apos;s connect and bring your vision to life. 😉👨‍💻"
         </p>
       </div>
       <div className={styles.cardSpace}>
         <LinkCard>Information Retrieval</LinkCard>
         <LinkCard>Information Retrieval</LinkCard>
         <LinkCard>Information Retrieval</LinkCard>
-      </div>
-      <div>
-        <p>I invest small angel checks into early-stage startups building tools for developers.</p>
       </div>
     </main>
   );
